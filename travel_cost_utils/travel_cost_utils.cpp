f66d76ad222aa1fe18d1820d3ad4fa6e944fbc47
Blake Mulnix <banjo@pop-os.localdomain>
2020-04-08 15:50:02 -0500

    add CMakeLists.txt and project code

---
+++ travel_cost_utils/travel_cost_utils.cpp
@@ -0,0 +1,26 @@
+#include "travel_cost_utils.hpp"
+#include <list>
+
+namespace TravelCostUtils
+{
+    double FuelCost(double numMiles, double milesPerGallon, double costPerGallon)
+    {
+        return numMiles / milesPerGallon * costPerGallon;
+    }
+
+    double LodgingCost(double costPerNight, double numNights)
+    {
+        return costPerNight * numNights;
+    }
+
+    double MiscCost(std::list<double> costs)
+    {
+        // Sum up misc costs
+        double sum = 0.0;
+        for (double cost : costs) {
+            sum += cost;
+        }
+
+        return sum;
+    }
+}
